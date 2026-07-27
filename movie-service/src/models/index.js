355aa2ecb95547a08d7fa3fb9ee4d59949843581
baha0x13 <bahaeddine.yahyaoui@insat.ucar.tn>
2026-07-27 23:55:18 +0100

    fix: pass DB_PORT through to Sequelize (was silently defaulting to 3306)

--- movie-service/src/models/index.js
+++ movie-service/src/models/index.js
@@ -3,6 +3,7 @@ const config = require('../config/db');
 
 const db = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
+    port: config.port,
     dialect: config.dialect,
     logging: config.logging,
     pool: {
